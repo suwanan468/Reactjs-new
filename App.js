cf22fa2e68c521e9d18449ff7c7dd199a5e27df1
suwanan468 <64253089+suwanan468@users.noreply.github.com>
2020-07-01 15:49:40 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css';
+import BoxCard from "./components/BoxTemperature/BoxCard"
+
+function App() {
+  return (
+    <div className="App">
+     <BoxCard />
+    </div>
+  );
+}
+
+export default App;
